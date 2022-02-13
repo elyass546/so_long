721bd40439e7e3f781e96bf8cbb0330eaecca97a
Ilias El aabbasi <ie-laabb@e1r10p8.1337.ma>
2022-02-13 21:49:30 +0100

    so_long

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,60 @@
 /*   By: ie-laabb <ie-laabb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 18:41:49 by ie-laabb          #+#    #+#             */
-/*   Updated: 2022/02/11 19:39:10 by ie-laabb         ###   ########.fr       */
+/*   Updated: 2022/02/13 13:07:39 by ie-laabb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "gnl.h"
+#include "so_long.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] > 0)
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char c)
+{
+    int		i;
+    char	*str;
+
+    i = 0;
+	str = malloc(ft_strlen(s1) + 2);
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = c;
+	str[i + 1] = '\0';
+	return (str);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*c;
+	int		i;
+	int		j;
+
+	i = ft_strlen(src);
+	j = 0;
+	c = (char *)malloc(sizeof(char) * (i + 1));
+	if (!c)
+		return (NULL);
+	while (src[j])
+	{
+		c[j] = src[j];
+		j++;
+	}
+	c[j] = '\0';
+	return (c);
+}
 
 char *get_next_line(int fd)
 {
@@ -20,8 +69,7 @@ char *get_next_line(int fd)
     int     read_return;
 
     read_return = read(fd, &c, 1);
-    str = malloc(1);
-    str[0] = '\0';
+    str = ft_strdup("");
     while (read_return > 0)
     {
         if (c == '\n')
@@ -29,7 +77,6 @@ char *get_next_line(int fd)
         temp = str;
         str = ft_strjoin(temp, c);
         free (temp);
-        
         read_return = read(fd, &c, 1);
     }
     if (read_return < 0)
@@ -37,7 +84,7 @@ char *get_next_line(int fd)
         free(str);
         return (NULL);
     }
-    if (read_return == 0)
+    if (read_return == 0 && !str)
     {
         free(str);
         return (NULL);
---
+++ so_long.h
@@ -0,0 +1,33 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <mlx.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char c);
+size_t	ft_strlen(char *str);
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+	int		height;
+	int		width;
+	int		height1;
+	int		width1;
+	void	*image_p;
+	void	*image_bg;
+}				t_vars;
+
+#endif
